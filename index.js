dd96131570869a4d1d204c80437aeb406c9de143
Muhammad Salman <salmanmanekia@gmail.com>
2020-02-25 14:43:36 +0200

    registering to the db

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const config = {
     host: 'iot-platform-postgresql.postgres.database.azure.com',
     // Do not hard code your username and password.
     // Consider using Node environment variables.
-    user: 'psql_user@iot-platform-postgresql',     
+    user: 'psql_user@iot-platform-postgresql',
     password: 'generic_password',
     database: 'iot',
     port: 5432,
@@ -21,7 +21,7 @@ const init = async () => {
     let data = [];
 
     const server = Hapi.server({
-        port: process.env.PORT || 3000,
+        port: process.env.PORT || 3001,
         host: "0.0.0.0"
     });
 
@@ -103,24 +103,32 @@ const init = async () => {
             const base64enc = reqPayload.data.body;
             const utf8enc = (new Buffer(base64enc, 'base64')).toString('utf8');
             const data = JSON.parse(utf8enc);
+
             let client = new Client(config);
-            try {
-                await client.connect();
-                const res = await client.query('INSERT INTO edge_devices(id) values($1)', [data.edgeDeviceId]);
-            } catch(ex) {
-                console.log(ex);
-            } finally {
-                await client.end();
-            }
-            
-            registerDevice(data.address, data.edgeDeviceId).then(async value => {
-                if(value.wasSuccessful) {
-                    client = new Client();
-                    await client.connect();
-                    const res = await client.query('INSERT INTO iot_devices(id, edge_device_id) values($1, $2)', [value.registrationId, value.edgeDeviceId]);
+            await client.connect();
+
+            const addressAvailableSql = await client.query('SELECT id from iot_devices where id = $1 and edge_device_id is null', [data.address]);
+            if(addressAvailableSql.rows.length === 1) {
+                try {
+                        //insert edge device
+                        const edge_res = await client.query('INSERT INTO edge_devices(id) values($1)', [data.edgeDeviceId]);
+                } catch(ex) {
+                    console.log(ex);
+                } finally {
                     await client.end();
                 }
-            });
+
+                registerDevice(data.address, data.edgeDeviceId).then(async value => {
+                    if(value.wasSuccessful) {
+                        client = new Client();
+                        await client.connect();
+                        const res = await client.query('INSERT INTO iot_devices(id, edge_device_id) values($1, $2)', [value.registrationId, value.edgeDeviceId]);
+                        await client.end();
+                    }
+                });
+            } else {
+
+            }
             return h.response().code(200);
         }
     });
@@ -135,6 +143,108 @@ const init = async () => {
             // console.log("--- PAYLOAD:")
             // console.log(request.payload)
             return h.response().code(200);
+        }
+    });
+
+    server.route({
+        method: "GET",
+        path: "/api/devices",
+        config: {
+            cors: {
+                "origin": ['*']
+            }
+        },
+        handler: async (request, h) => {
+            const client = new Client();
+            await client.connect();
+            const sqlres = await client.query("Select id, address, edge_device_id from iot_devices;");
+            await client.end();
+
+            const response = h.response(sqlres.rows);
+            response.code(200)
+            response.type("application/json");
+            return response;
+        }
+    });
+
+    server.route({
+        method: "POST",
+        path: "/api/devices",
+        config: {
+            cors: {
+                "origin": ['*']
+            }
+        },
+        handler: async (request, h) => {
+            const {data} = request.payload;
+            const response = h.response();
+
+            //console.log(data);
+            try {
+                const client = new Client();
+                await client.connect();
+                const sqlres = await client.query("INSERT INTO iot_devices(id) VALUES($1)", [data]);
+                await client.end();
+
+                //console.log(request.payload);
+
+
+            }
+            catch(ex) {
+                console.log(ex);
+                if(ex.detail && ex.detail.includes("already exists")) {
+                    response.code(409);
+                } else {
+                    response.code(400);
+                }
+                return response;
+            }
+            response.code(200);
+            return response;
+        }
+    });
+
+    server.route({
+        method: "GET",
+        path: "/api/edges",
+        config: {
+            cors: {
+                "origin": ['*']
+            }
+        },
+        handler: async (request, h) => {
+            //return ({ addresses: data.map(({ address }) => address) });
+            const client = new Client();
+            await client.connect();
+            const sqlres = await client.query("Select id from edge_devices;");
+            await client.end();
+
+            const response = h.response(sqlres.rows);
+            response.code(200)
+            response.type("application/json");
+            return response;
+        }
+    });
+
+    server.route({
+        method: "POST",
+        path: "/api/edges",
+        config: {
+            cors: {
+                "origin": ['*']
+            }
+        },
+        handler: async (request, h) => {
+            //return ({ addresses: data.map(({ address }) => address) });
+            const client = new Client();
+            await client.connect();
+            const sqlres = await client.query("Select id from iot_edge_devices;");
+            await client.end();
+
+            const response = h.response(sqlres.rows);
+            response.code(200)
+            response.type("application/json");
+            return response;
         }
     });
 
