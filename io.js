c28d12bee396e5840ff8351577cb53271b286fa0
Muhammad Salman <salmanmanekia@gmail.com>
2020-04-17 01:56:38 +0300

    remove unused function and typo fix

--- io.js
+++ io.js
@@ -2,14 +2,11 @@ const sio = require('socket.io');
 
 let io = null;
 const ids = new Map();
-exports.get = function() {
-    return io;
-}
 
-exports.initialize = function(server) {
+exports.initialize = function (server) {
     io = sio.listen(server.listener);
     io.on('connection', (socket) => {
-        console.log(`A user conected with ${socket.id}`);
+        console.log(`A user connected with ${socket.id}`);
         socket.on('UPDATE_DEVICE_SELECTION', (data) => {
             console.log(`UPDATE DEVICE SELECTION by socket id ${socket.id} for device ${data.deviceId}`);
             data.prop === "ADD" && ids.set(socket.id, data.deviceId);
@@ -21,9 +18,9 @@ exports.initialize = function(server) {
     })
 }
 
-exports.sendDeviceData = function(currentDevice, data) {
+exports.sendDeviceData = function (currentDevice, data) {
     for (const [socketId, deviceId] of ids.entries()) {
-        if(deviceId === currentDevice) {
+        if (deviceId === currentDevice) {
             console.log('SENDING DEVICE DATA of ', currentDevice);
             io.to(socketId).emit('DEVICE_DATA', data);
         }
